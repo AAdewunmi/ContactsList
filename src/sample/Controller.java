a8c7420eea826cc3c3a3896677314e364302bca6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2021-08-27 14:01:08 +0100

    Fourth Commit: Add new contact functionality implemented.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,74 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import sample.datamodel.Contact;
+import sample.datamodel.ContactData;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Optional;
 
 public class Controller {
+
+    @FXML
+    private BorderPane mainPanel;
+
+    @FXML
+    private TableView<Contact> contactsTable;
+
+    @FXML
+    private ContactData data;
+
+    @FXML
+    public void initialize(){
+        data = new ContactData();
+        data.loadContacts();
+        contactsTable.setItems(data.getContacts());
+    }
+
     @FXML
     public void showAddContactDialog() {
+        Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+        dialog.initOwner(mainPanel.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactdialog.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        }catch (IOException exception){
+            System.out.println("Couldn't load the dialog!");
+            exception.printStackTrace();
+            return;
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            ContactController contactController = fxmlLoader.getController();
+            Contact newContact = contactController.getNewContact();
+            data.addContact(newContact);
+            data.saveContacts();
+        }
+    }
+
+    @FXML
+    public void showEditContactDialog() {
+        Contact selectedContact = contactsTable.getSelectionModel().getSelectedItem();
+        if (selectedContact == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No Contact Selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select the contact you want to edit");
+            alert.showAndWait();
+            return;
+        }
     }
 }
